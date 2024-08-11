9e73bcd81ea6274dd36c48ce55efdce92d6d8ed6
Elijah-Vakulenko <154387383+Elijah-Vakulenko@users.noreply.github.com>
2024-08-11 12:29:48 +0100

    folders added

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { GiRotaryPhone } from "react-icons/gi";
+import ContactForm from './ContactForm/ContactForm';
+import SearchBox from './SearchBox/SearchBox';
+import ContactList from './ContactList/ContactList';
+import s from './App.module.css';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <div className={s.wrapper}>
+      <div className={s.logo}>
+        <GiRotaryPhone className={s.icon} />
+        <h1 className={s.title}><span>P</span>honebook</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
   )
 }
 
